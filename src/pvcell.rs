a699ec268a5e5da698d640498048ed465ef041f6
j-Lago <98105937+j-Lago@users.noreply.github.com>
2025-06-03 13:39:52 -0300

    initial

---
+++ src/pvcell.rs
@@ -0,0 +1,189 @@
+
+
+
+const T_REF: f64 = 298.15; // [K] rated temperature
+const S_REF: f64 = 1000.0; // [W/m^2] rated irradiance
+const Q_K: f64 = 1.60217663e-19 / 1.38064852e-23;  // [K/eV] Boltzmann constante reciprocal
+const C_TO_K: f64 = 273.15;
+
+use std::fmt;
+use tracing::{warn, error};
+
+pub static mut SOLVER_CALLS: usize = 0;
+
+
+#[derive(Clone)]
+pub struct PvCellSolver {
+    pub max_iter: usize,    // max number of iterations
+    pub tol_i: f64, // [A] current tolerance
+    pub tol_v: f64, // [V] voltage tolerance
+}
+
+impl Default for PvCellSolver {
+    fn default() -> Self {
+        return PvCellSolver { max_iter: 100, tol_i: 0.001, tol_v: 0.01 };
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct CellState{
+    pub gsh: f64,
+    pub ra: f64,
+    pub i0: f64,
+    pub il: f64,
+}
+
+pub enum Params{
+    Basic { a_ref: f64, i_o_ref: f64, i_l_ref: f64, r_s: f64, r_sh_ref: f64, alpha_sc: f64, v_oc_ref: f64 },
+    Extended { a_ref: f64, i_o_ref: f64, i_l_ref: f64, r_s: f64, r_sh_ref: f64, alpha_sc: f64, v_oc_ref: f64, v_bypass: f64, r_bypass: f64, eg_ref: f64, degdt: f64 }
+}
+
+#[derive(Clone)]
+pub struct PvCell {
+    pub a_ref: f64,
+    pub i_o_ref: f64,
+    pub i_l_ref: f64,
+    pub r_s: f64,
+    pub r_sh_ref: f64,
+    pub alpha_sc: f64,
+    pub v_oc_ref: f64,
+    pub v_bypass: f64, // [V] tensão de bypass
+    pub r_bypass: f64, // [Ohm] resistance of bypass diode
+    pub eg_ref: f64,   // [eV]  band energy Si: 1.121, CdTe: 1.475
+    pub degdt: f64,    // Si: -0.0002677, CdTe: -0.0003 //
+    pub shading: f64,
+    pub np: u32, // [-]   number of modules in parallel
+    pub ns: u32, // [-]   number of modules in series
+    pub solver: PvCellSolver,
+}
+
+#[allow(dead_code)]
+impl PvCell {
+    pub fn new(params: &Params) -> Self {
+        match params {
+            &Params::Basic{a_ref, i_o_ref, i_l_ref, r_s, r_sh_ref, alpha_sc, v_oc_ref} => {
+                PvCell {
+                    a_ref, i_o_ref, i_l_ref, r_s, r_sh_ref, alpha_sc, v_oc_ref,
+                    v_bypass: -0.65 * 3.0, r_bypass: 0.1, eg_ref: 1.121, degdt: -0.0002677,
+                    shading: 0.0, np: 1, ns: 1, solver: PvCellSolver::default()
+                }
+            }
+            &Params::Extended{a_ref, i_o_ref, i_l_ref, r_s, r_sh_ref, alpha_sc, v_oc_ref,
+                v_bypass, r_bypass, eg_ref, degdt} => {
+                PvCell {
+                    a_ref, i_o_ref, i_l_ref, r_s, r_sh_ref, alpha_sc, v_oc_ref,
+                    v_bypass, r_bypass, eg_ref, degdt,
+                    shading: 0.0, np: 1, ns: 1, solver: PvCellSolver::default()
+                }
+            }
+        }
+    }
+
+    /// builders
+    pub fn with_ns(mut self, ns: u32) -> Self{ self.ns = ns; return self; }
+    pub fn with_np(mut self, np: u32) -> Self{ self.np = np; return self; }
+    pub fn with_shading(mut self, shading: f64) -> Self{ self.shading = shading; return self; }
+    pub fn with_solver(mut self, settings: PvCellSolver) -> Self { self.solver = settings; return self; }
+
+    pub fn compute_state(&self, irrad_ef: f64, cell_temp: f64) -> CellState {
+        let irrad: f64 = irrad_ef * (1.0 - self.shading);
+        let tj: f64 = cell_temp + C_TO_K;
+        let eg: f64 = self.eg_ref * (1. + self.degdt  * (tj - T_REF));
+        let gsh: f64 = irrad / (self.r_sh_ref * S_REF);
+        let ra: f64 = T_REF / (self.a_ref * tj);
+        let i0: f64 = self.i_o_ref  * (tj / T_REF).powi(3) * (Q_K * (self.eg_ref / T_REF - eg / tj)).exp();
+        let il: f64 = (self.i_l_ref + self.alpha_sc * (tj - T_REF)) * irrad / S_REF;
+        CellState{gsh, ra, i0, il}
+    }
+
+    pub fn solve_i(&self, state: &CellState, v_pnl: f64) -> f64 {
+        unsafe{ SOLVER_CALLS += 1}
+        let mut i: f64 = 0.0;
+        let v: f64 = v_pnl / (self.ns as f64);
+
+        let mut success: bool = false;
+        for _ in 0..self.solver.max_iter {
+            let den: f64 = -1.0 - state.i0 * ((v + i * self.r_s) * state.ra).exp() * self.r_s * state.ra - self.r_s * state.gsh;
+            let d: f64 = (state.il - i - state.i0 * (((v + i * self.r_s) * state.ra).exp() - 1.0) - (v + i * self.r_s) * state.gsh) / den;
+            i = i - d;
+            if d.abs() < self.solver.tol_i {
+                success = true;
+                break;
+            }
+        }
+        if v < self.v_bypass {
+            i += (self.v_bypass - v) / self.r_bypass;
+        }
+        i *= self.np as f64;
+
+        if !success {
+            if i.is_normal() {
+                warn!("({:p}) PvCell::solve_i(v_pnl={:e}) nao convergiu (tol={:e}, max_iter={}) -> (i={})",
+                    &self, v_pnl, self.solver.tol_i, self.solver.max_iter, i);
+            }else{
+                error!("({:p}) PvCell::solve_i(v_pnl={:e}) nao convergiu (tol={:e}, max_iter={}) -> (i={})",
+                    &self, v_pnl, self.solver.tol_i, self.solver.max_iter, i);
+            }
+        }
+        return i;
+    }
+
+    pub fn v_from_i(&self, state: &CellState, i_pnl: f64) -> f64 {
+        unsafe{ SOLVER_CALLS += 1}
+        let mut v: f64 = self.v_oc_ref;
+        let i: f64 = i_pnl / (self.np as f64);
+
+        let mut success = false;
+        for _ in 0..self.solver.max_iter {
+            let den: f64 = -state.i0 * ((v + i * self.r_s) * state.ra).exp() * state.ra - (state.gsh);
+            let d: f64 = (state.il - i - state.i0 * (((v + i * self.r_s) * state.ra).exp() - 1.) - (v + i * self.r_s) * state.gsh) / den;
+            v -= d;
+            if d.abs() < self.solver.tol_v {
+                success = true;
+                break;
+            }
+        }
+        if v < self.v_bypass {
+            let ir: f64 = self.solve_i(state, self.v_bypass) / (self.np as f64);   // corrente em que se inicia região de breakdown para solve_v(i)
+            v = self.v_bypass - (i - ir) * self.r_bypass;
+        }
+        v *=  self.ns as f64;
+
+        if !success {
+            if v.is_normal() {
+                warn!("({:p}) PvCell::v_from_i(i_pnl={:e}) nao convergiu (tol={:e}, max_iter={}) -> (v={})",
+                    &self, i_pnl, self.solver.tol_v, self.solver.max_iter, v);
+            }else{
+                error!("({:p}) PvCell::v_from_i(i_pnl={:e}) nao convergiu (tol={:e}, max_iter={}) -> (v={})",
+                    &self, i_pnl, self.solver.tol_v, self.solver.max_iter, v);
+            }
+        }
+        return v;
+    }
+
+    pub fn is_series_equivalent(&self, other: &PvCell) -> bool {
+        return
+            self.a_ref == other.a_ref &&
+            self.i_o_ref == other.i_o_ref &&
+            self.i_l_ref == other.i_l_ref &&
+            self.r_s == other.r_s &&
+            self.r_sh_ref == other.r_sh_ref &&
+            self.alpha_sc == other.alpha_sc &&
+            self.v_oc_ref == other.v_oc_ref &&
+            self.v_bypass == other.v_bypass &&
+            self.r_bypass == other.r_bypass &&
+            self.eg_ref == other.eg_ref &&
+            self.degdt == other.degdt &&
+            self.shading == other.shading &&
+            // self.ns == other.ns &&
+            self.np == other.np
+            // self.solver_settings == other.solver_settings;
+    }
+}
+
+
+impl fmt::Debug for PvCell {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{{{},{}}}", self.ns, self.np)
+    }
+}
---
+++ src/series.rs
@@ -0,0 +1,200 @@
+
+use crate::pvcell::{CellState, PvCell};
+use std::ops::Index;
+use std::iter::IntoIterator;
+use tracing::{warn, error};
+use std::fmt;
+
+pub static mut SOLVER_CALLS: usize = 0;
+
+pub struct SeriesSolver {
+    pub max_iter: usize,
+    pub tol_v: f64,
+    pub min_g: f64,
+}
+
+impl Default for SeriesSolver {
+    fn default() -> Self {
+        SeriesSolver {
+            max_iter: 1000,
+            tol_v: 0.1,
+            min_g: 0.00001,
+        }
+    }
+}
+
+pub struct Series {
+    elements: Vec<PvCell>,
+    solver: SeriesSolver,
+}
+
+#[allow(dead_code)]
+impl Series {
+    pub fn empty() -> Series {
+        return Series {
+            elements: vec![],
+            solver: SeriesSolver::default()
+        };
+    }
+
+    pub fn new(panels: Vec<PvCell>) -> Series {
+        return Series {
+            elements: panels,
+            solver: SeriesSolver::default()
+        };
+    }
+
+    /// builder: let s: Series = Series::new(...).with_solver(...);
+    pub fn with_solver(mut self, settings: SeriesSolver) -> Series { self.solver = settings; return self; }
+
+    pub fn len(&self) -> usize {
+        return self.elements.len();
+    }
+
+    pub fn iter(&self) -> impl Iterator<Item = &PvCell> {
+        self.elements.iter()
+    }
+
+    pub fn push(&mut self, element: PvCell){
+        self.elements.push(element);
+    }
+
+    pub fn states_uniform_conditions(&self, irrad_ef: f64, cell_temp: f64) -> Vec<CellState> {
+        let mut states: Vec<CellState> = Vec::with_capacity(self.len());
+        for pnl in self.elements.iter(){
+            states.push(pnl.compute_state(irrad_ef, cell_temp));
+        }
+        return states;
+    }
+
+    pub fn vs_from_i(&self, states: &Vec<CellState>, i: f64) -> Vec<f64> {
+        let mut voltages: Vec<f64> = Vec::with_capacity(self.len());
+        for (k, pnl) in self.iter().enumerate(){
+            voltages.push(pnl.v_from_i(&states[k], i))
+        }
+        voltages
+    }
+
+    pub fn v_from_i(&self, states: &Vec<CellState>, i: f64) -> f64 {
+        let voltages = self.vs_from_i(states, i);
+        voltages.iter().sum()
+    }
+
+    pub fn i_from_v(&self, states: &Vec<CellState>, v_str: f64) -> f64 {
+        unsafe{ SOLVER_CALLS += 1; }
+
+        let mut sum_voc: f64 = 0.0;
+        let mut il: f64 = f64::INFINITY;
+        let mut i0: f64 = f64::INFINITY;
+        for (k, pnl) in self.elements.iter().enumerate() {
+            sum_voc += pnl.v_oc_ref * (pnl.ns as f64);
+            il = il.min(pnl.i_l_ref * (pnl.np as f64));
+            i0 = i0.min(pnl.solve_i(&states[k], 0.0));
+        }
+        let mut g: f64 = il / sum_voc;
+        // let mut g: f64 = 10.0 / sum_voc;
+
+        let mut dv1: f64 = 0.0;
+        for _ in 0..self.solver.max_iter {
+            let v0 = self.v_from_i(states, i0);
+            let dv = v0 - v_str;
+            if dv.abs() < self.solver.tol_v {
+                return i0;
+            }
+            if dv * dv1 < 0.0 {  // mudança de sinal
+                g /= 2.0;
+            }
+            g = g.max(self.solver.min_g);
+            i0 += dv * g;
+            dv1 = dv;
+        }
+
+        if i0.is_normal() {
+            warn!("({:p}) Series::i_from_v(v_str={:e}) nao convergiu (tol={:e}, max_iter={}) -> (i={})",
+                &self, v_str, self.solver.tol_v, self.solver.max_iter, i0);
+        } else {
+            error!("({:p}) Series::i_from_v(v_str={:e}) nao convergiu (tol={:e}, max_iter={}) -> (i={})",
+                &self, v_str, self.solver.tol_v, self.solver.max_iter, i0);
+        }
+        return i0;
+    }
+
+    pub fn find(&self, other: &PvCell) -> Option<usize> {
+        for (k, pnl) in self.elements.iter().enumerate(){
+            if pnl.is_series_equivalent(other) {
+                return Some(k);
+            }
+        }
+        return None;
+    }
+
+    pub fn reduce(&self) -> (Series, Vec<u32>, Vec<Vec<u32>>) {
+        let mut reduced: Series = Series::empty();
+        let mut origin_to_reduced: Vec<u32> = vec![0; self.len()];
+        let mut reduced_to_origin: Vec<Vec<u32>> = vec![];
+
+        for i in 0..self.elements.len() {
+            match reduced.find(&self.elements[i]) {
+                Some(j) => {
+                    reduced.elements[j].ns += self.elements[i].ns;
+                    reduced_to_origin[j].push(i as u32);
+                    origin_to_reduced[i] = j as u32;
+                }
+                None => {
+                    origin_to_reduced[i] = reduced.elements.len() as u32;
+                    reduced.elements.push(self.elements[i].clone());
+                    reduced_to_origin.push(vec![i as u32]);
+                }
+            }
+        }
+        return (reduced, origin_to_reduced, reduced_to_origin);
+    }
+}
+
+impl fmt::Debug for Series {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "PvString {:?}", &self.elements)
+    }
+}
+
+impl Index<usize> for Series {
+    type Output = PvCell;
+    fn index(&self, index: usize) -> &Self::Output {
+        &self.elements[index]
+    }
+}
+
+impl IntoIterator for Series {
+    type Item = PvCell;
+    type IntoIter = std::vec::IntoIter<PvCell>;
+    fn into_iter(self) -> Self::IntoIter {
+        return self.elements.into_iter();
+    }
+}
+
+// impl StringStates{
+//     #[allow(dead_code)]
+//     pub fn iter(&self) -> impl Iterator<Item = &CellState> {
+//         self.elements.iter()
+//     }
+//
+//     #[allow(dead_code)]
+//     fn len(&self) -> usize {
+//         self.elements.len()
+//     }
+// }
+
+// impl IntoIterator for StringStates {
+//     type Item = CellState;
+//     type IntoIter = std::vec::IntoIter<CellState>;
+//     fn into_iter(self) -> Self::IntoIter {
+//         return self.elements.into_iter();
+//     }
+// }
+
+// impl Index<usize> for StringStates {
+//     type Output = CellState;
+//     fn index(&self, i: usize) -> &Self::Output {
+//         &self.elements[i]
+//     }
+// }
